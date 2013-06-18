627731842b99c1e8214f0fe268dfd817707881ba
Laurent Dami <dami@cpan.org>
2013-06-18 04:58:27 +0200

    #86219: prevent prevent "where_on" conditions from accumulating in global hash.

--- lib/DBIx/DataModel/Statement.pm
+++ lib/DBIx/DataModel/Statement.pm
@@ -12,6 +12,7 @@ use Scalar::Util     qw/weaken refaddr reftype dualvar/;
 use Params::Validate qw/validate ARRAYREF HASHREF/;
 use POSIX            qw/LONG_MAX/;
 use Acme::Damn       qw/damn/;
+use Clone            qw/clone/;
 use Try::Tiny;
 
 use DBIx::DataModel;
@@ -259,7 +260,7 @@ sub sqlize {
                         -union -union_all -intersect -except -minus
                         -order_by -group_by -having
                         -limit -offset -page_size -page_index/;
-  my %sqla_args = (-from         => $meta_source->db_from,
+  my %sqla_args = (-from         => clone($meta_source->db_from),
                    -want_details => 1);
   $args->{$_} and $sqla_args{$_} = $args->{$_} for @args_to_copy;
   $sqla_args{-columns} ||= $meta_source->default_columns;
