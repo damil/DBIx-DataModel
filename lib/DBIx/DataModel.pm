0e6e4a8549b25b37c64f8f4beb0b8a20cd65df72
Laurent Dami <dami@cpan.org>
2018-01-15 10:02:08 +0100

    bump to 2.47_07

--- lib/DBIx/DataModel.pm
+++ lib/DBIx/DataModel.pm
@@ -11,7 +11,7 @@ use MRO::Compat;
 use DBIx::DataModel::Meta::Utils qw/does/;
 use Carp::Clan qw[^(DBIx::DataModel::|SQL::Abstract)];
 
-our $VERSION = '2.47_06';
+our $VERSION = '2.47_07';
 
 # compatibility setting : see import()
 our $COMPATIBILITY = $VERSION; # from 2.20, no longer automatic compatibility
